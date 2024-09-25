5948b1dc80419fa1a164cf8048cdbebfea2f6ab6
vizssouza <vini.nelso@gmail.com>
2024-09-25 17:22:39 -0300

    Adicionando animações

--- js/scripts.js
+++ js/scripts.js
@@ -44,3 +44,21 @@ document.querySelectorAll('.carousel-button').forEach(e => e.addEventListener('c
     behavior: 'smooth',
   });
 }));
+
+// Animações
+// Coletando o observador da página
+const myObserver = new IntersectionObserver( (entries) => {
+  entries.forEach( (entry) => {
+      if (entry.isIntersecting === true){
+          entry.target.classList.add('show')
+      } else {
+          entry.target.classList.remove('show')
+      }
+  })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach( (element) => myObserver.observe(element))
